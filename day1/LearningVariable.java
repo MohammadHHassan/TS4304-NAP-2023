62a043eed081fac9f014228e350c5839f379646f
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2023-07-24 11:56:24 +0800

    Add files via upload

---
+++ day1/LearningVariable.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class LearningVariable
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		double b = 2.5;
+		char c = 'A';
+		String d = "Mohammad 123 !@#";
+		boolean e = true;
+		
+		System.out.print("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d + ", e = " + e);
+//		System.out.print(", b = " + b);
+//		System.out.print(", c = " + c);
+//		System.out.print(", d = " + d);
+//		System.out.print(", e = " + e);
+	}
+}
