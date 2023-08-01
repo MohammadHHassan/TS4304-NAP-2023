ecb91e86a656163949c5ac34073f4b0b10b7c21e
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2023-08-01 14:35:57 +0800

    Add files via upload

---
+++ day4/LearningWhile.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class LearningWhile
+{
+	public static void main(String[] args)
+	{
+		int i=1;
+		
+		while(i<=10)
+		{
+			System.out.println(i + ". Welcome to Java Programming!");
+			i++;
+		}
+		
+		System.out.println("Thank you :)");
+	}
+}
---
+++ day4/LearningWhileWithScanner.java
@@ -0,0 +1,31 @@
+package day4;
+
+import java.util.Scanner;		// Import Scanner API/library
+
+public class LearningWhileWithScanner
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);	// Implement a Scanner class to accept user's input
+		
+		System.out.print("Please enter the number 5: ");	// Prompt a message for the user to enter the number 5
+		int value = scanner.nextInt();		// Store the user's input in variable value (int)
+		
+		int count=0;		// Declare the variable count to count the user's wrong input
+		
+		while(value!=5)		// While condition value is not equal to 5
+		{
+			System.out.println("You have entered the wrong value.");
+			System.out.print("Please try again: ");		// Ask the user to enter another value
+			value = scanner.nextInt();		// Store the user's input in value
+			count++;		// Increment count
+		}
+		
+		System.out.println("You have entered the correct value.");
+		System.out.println("Number of times you entered an incorrect value: " + count);
+		
+		scanner.close();
+	}
+}
+
+
