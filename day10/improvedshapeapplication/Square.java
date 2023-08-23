b42f9815d800a5355ede4f8c4541343b8c49e84f
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2023-08-23 14:55:05 +0800

    Add files via upload

---
+++ day10/improvedshapeapplication/Square.java
@@ -0,0 +1,31 @@
+package day10.improvedshapeapplication;
+
+public class Square extends Shape
+{
+	double side;
+	
+	public Square(String colour, double side)
+	{
+		super(colour);
+		this.side = side;
+	}
+
+	@Override
+	public double getArea()
+	{
+		return side*side;
+	}
+
+	@Override
+	public double getPerimeter()
+	{
+		return 4*side;
+	}
+
+	@Override
+	public String toString() {
+		return "Square [ Side = " + side + ", Colour = " + colour + ", Area = " + df.format(getArea()) + ", Perimeter = "
+				+ df.format(getPerimeter()) + " ]";
+	}
+	
+}
